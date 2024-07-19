28879728a266e366c958735919abffd3ba522b53
naresh2002 <naresh6436@gmail.com>
2024-07-19 18:55:27 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -1,31 +1,5 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import About from './components/About';
 import Education from './components/Education';
@@ -35,17 +9,21 @@ import Achievements from './components/Achievements';
 import Additional from './components/Additional';
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <About />
-            <Education />
-            <Experience />
-            <Projects />
-            <Achievements />
-            <Additional />
-        </div>
-    );
+  return (
+    <Router basename="/<your-repo-name>">
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route path="/education" component={Education} />
+          <Route path="/experience" component={Experience} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/achievements" component={Achievements} />
+          <Route path="/additional" component={Additional} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
